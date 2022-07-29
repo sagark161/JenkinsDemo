3fe652f217b82261c73cf226983e0db0e80cc8bb
sagark161 <sagar.k161@gmail.com>
2022-07-29 09:30:37 +0530

    First commit

---
+++ src/test/java/module1/Test1.java
@@ -0,0 +1,18 @@
+package module1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	void case1(){
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	void case2(){
+		Assert.assertTrue(false);
+	}
+
+}
